698ba445c2e2e24ed99174dfcb15e35773c9bc5e
Emir <e.navruzov@ktsstudio.ru>
2019-10-06 16:52:22 +0300

    edited timeout for sh

--- scripts/aioserver_setup.sh
+++ scripts/aioserver_setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 /code/scripts/wait-for-it.sh db:5432
-/code/scripts/wait-for-it.sh rmq:5672
+/code/scripts/wait-for-it.sh --timeout=0 rmq:5672
 
 python -m aioserver.server
--- scripts/crawler_setup.sh
+++ scripts/crawler_setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 /code/scripts/wait-for-it.sh db:5432
-/code/scripts/wait-for-it.sh rmq:5672
-/code/scripts/wait-for-it.sh es:9200
+/code/scripts/wait-for-it.sh --timeout=0 rmq:5672
+/code/scripts/wait-for-it.sh --timeout=0 es:9200
 
 python -m crawler.main
