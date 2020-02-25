2fd3d900cd054ba7c03e736fa3cb623bf6a74d3c
Emir <e.navruzov@ktsstudio.ru>
2020-02-25 20:08:51 +0300

    kube finished

--- scripts/aioserver_setup.sh
+++ scripts/aioserver_setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-/code/scripts/wait-for-it.sh db:5432
-/code/scripts/wait-for-it.sh --timeout=0 rmq:5672
+/code/scripts/wait-for-it.sh $POSTGRES_HOST_DB:5432
+/code/scripts/wait-for-it.sh --timeout=0 $HOST_RABBITMQ:5672
 
 python -m aioserver.server
--- scripts/crawler_setup.sh
+++ scripts/crawler_setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-/code/scripts/wait-for-it.sh db:5432
-/code/scripts/wait-for-it.sh --timeout=0 rmq:5672
-/code/scripts/wait-for-it.sh --timeout=0 es:9200
+/code/scripts/wait-for-it.sh $POSTGRES_HOST_DB:5432
+/code/scripts/wait-for-it.sh --timeout=0 $HOST_RABBITMQ:5672
+/code/scripts/wait-for-it.sh --timeout=0 $HOST_ES:9200
 
 python -m crawler.main
